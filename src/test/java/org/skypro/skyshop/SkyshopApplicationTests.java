d5f446cd90f2de083b193fd507af6c9c5fbd9fe1
Кирик <kmaibo@mail.ru>
2025-12-08 21:30:11 +0300

    completed

---
+++ src/test/java/org/skypro/skyshop/SkyshopApplicationTests.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+@SpringBootTest
+class SkyshopApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyshopApplicationTests.class, args);
+    }
+
+	@Test
+	void contextLoads() {
+	}
+
+}
